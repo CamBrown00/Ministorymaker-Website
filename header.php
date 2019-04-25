ab1f58c8a27829e9cf612ae30b893dbc865d129d
squidknits <sabertra@uvm.edu>
2019-04-25 12:03:46 -0400

    Work on index html layout, creation of separate css files

--- header.php
+++ header.php
@@ -1,7 +1,6 @@
 <!-- ======================== Start of Header ======================== -->
 <header>
-    <img class="logo" alt="Header Image" src="images/header.png">
-    <h1 class='site-header'>Placeholder Header</h1>
+    <h1 class='site-header'><a href=index.php>Placeholder Header</a></h1>
     <h3 class='tagline'>Placeholder Tag line</h3>
 </header>
 <!-- ======================== End of Header ======================== -->
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <main>
         <article>
             <p class='intro'>
+                <img class="home-img-1" alt="" src="images/home-img-1.jpg">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean 
                 finibus rhoncus nunc. Nulla facilisi. Ut ac gravida mauris, eu 
                 dictum sapien. Nam blandit tincidunt nisi ac euismod. Sed justo est,
@@ -12,11 +13,21 @@
                 suscipit. Mauris cursus erat ac malesuada rhoncus.
             </p>   
         </article>
+        <article>
+            <img class="home-img-2" alt="" src="images/home-img-2.jpg">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean 
+                finibus rhoncus nunc. Nulla facilisi. Ut ac gravida mauris, eu 
+                dictum sapien. Nam blandit tincidunt nisi ac euismod. Sed justo est,
+                gravida ut tellus ut, varius pellentesque tortor. Vestibulum laoreet 
+                ipsum et enim aliquet tincidunt. In vulputate leo eu magna faucibus 
+                suscipit. Mauris cursus erat ac malesuada rhoncus.
+            </p>
+        </article>
     </main>
 
     <!-- Contents of csv file are displayed here -->
     <?php
-        include('storySets.php');
         include('footer.php');
     ?>
 </body>
