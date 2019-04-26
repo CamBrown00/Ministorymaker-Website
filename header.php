53f51f30242758300f4ca437a99d8f73c0a4e911
squidknits <sabertra@uvm.edu>
2019-04-25 20:20:52 -0400

    Work on header

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <!-- ======================== Start of Header ======================== -->
 <header>
-    <h1 class='site-header'><a href=index.php>Placeholder Header</a></h1>
-    <h3 class='tagline'>Placeholder Tag line</h3>
+    <div id="header-wrapper">
+        <h1 class='site-header'><a href=index.php>Placeholder Header</a></h1>
+        <h3 class='tagline'>Placeholder Tag line</h3>
+    </div>
 </header>
 <!-- ======================== End of Header ======================== -->
