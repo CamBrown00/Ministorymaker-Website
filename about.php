84e312089c67bee69770a83d4c719e7bd6a366d6
cameroo52 <ccbrown@uvm.edu>
2019-05-02 00:05:00 -0700

    Styled two thirds of the about page and added some mobile compatibility

--- about.php
+++ about.php
@@ -11,12 +11,13 @@
     </p>
 </section>
 
-<section class="aboutDetails">
-    <h2 class="heading2">How to Use the Mini Story Maker</h2>
-    <p class ="aboutText">
+<main class="aboutDetails">
+    <section>
+    <h2>How to Use</h2>
+    <p class ="helpText">
         Using the site is simple!<br>
         If you want to make a story,
-        <ol>
+        <ul>
             <li>
                 Click on the Make a Story button
             </li>
@@ -26,39 +27,49 @@
             <li>
                 Submit the story!
             </li>
-        </ol>
+        </ul>
     </p>
-
-    <h2 class="heading2">Help Info</h2>
+    </section>
+    
+    <section>
+    <h2>Contact Information</h2>
     <p class ="aboutText">
-        Confused by how to use the site?
+        If you want to contact us, whether it be for business inquiries or
+        just a friendly hello, feel free to send us an email at
         <a href="mailto:ministorymaker1@gmail.com">
-            Feel free to email us if you need any help!
+            ministorymaker1@gmail.com
         </a>
     </p>
     <p class="aboutText">
         We're always open to suggestions or criticism, so visit our 
         <a href="feedback.php">feedback page</a> to submit a form if you'd like.
     </p>
-</section>
-<section class="devBios">
+    </section>
+</main>
+<main class="devBios">
     <h2 class="heading2">The Developers</h2>
-    <h3>Sydney</h3>
-    <img class="bioImg" alt="" src="images/sydneyBio.png">
-    <p class="bio">
-        I’m a Computer Science student at the University of Vermont. My personal interests include coding games, making music, and playing video games. I worked on the Read-a-Story and About pages, and helped work on the style of the site.
-    </p>
-    <h3>AriaRay</h3>
-    <img class="bioImg" alt="" src="images/ariaBio.png">
-    <p class="bio">
-        I’m a Computer Science student at the University of Vermont. My personal interests include coding games, making music, and playing video games. I worked on the Read-a-Story and About pages, and helped work on the style of the site.
-    </p>
-    <h3>Cam</h3>
-    <img class="bioImg" alt="" src="images/camBio.png">
-    <p class="bio">
-        I’m a Computer Science student at the University of Vermont. My personal interests include coding games, making music, and playing video games. I worked on the Read-a-Story and About pages, and helped work on the style of the site.
-    </p>
-</section>
+    <section>
+        <h3>Sydney</h3>
+        <img class="bioImg" alt="" src="images/sydneyBio.png">
+        <p class="bio">
+            I’m a Computer Science student at the University of Vermont. My personal interests include coding games, making music, and playing video games. I worked on the Read-a-Story and About pages, and helped work on the style of the site.
+        </p>
+    </section>
+    <section>
+        <h3>AriaRay</h3>
+        <img class="bioImg" alt="" src="images/ariaBio.png">
+        <p class="bio">
+            I’m a Computer Science student at the University of Vermont. My personal interests include coding games, making music, and playing video games. I worked on the Read-a-Story and About pages, and helped work on the style of the site.
+        </p>
+    </section>
+    <section>
+        <h3>Cam</h3>
+        <img class="bioImg" alt="" src="images/camBio.png">
+        <p class="bio">
+            I’m a Computer Science student at the University of Vermont. My personal interests include coding games, making music, and playing video games. I worked on the Read-a-Story and About pages, and helped work on the style of the site.
+        </p>
+    </section>
+</main>
 
 <?php
     include('footer.php');
