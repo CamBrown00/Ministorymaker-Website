28b2d44d65d678270211c985660cdb78cce68d0b
cameroo52 <ccbrown@uvm.edu>
2019-05-01 18:37:19 -0700

    Wrote first half of content for the about page

--- about.php
+++ about.php
@@ -2,7 +2,41 @@
     include('top.php');
 ?>
 
+<section class="introSection">
+    <h1 class="introHeading">"What is Mini Story Maker?"</h1>
+    <p class ="aboutText">
+        We're a web site devoted to bringing an interactive aspect to reading
+        classic children's stories. If you're also interested in reading the 
+        original stories themselves, we got that covered too!
+    </p>
+</section>
 
+<section class="aboutDetails">
+    <h2 class="heading2">How to Use the Mini Story Maker</h2>
+    <p class ="aboutText">
+        Using the site is simple!<br>
+        If you want to make a story,
+        <ol>
+            <li>
+                Click on the Make a Story button
+            </li>
+            <li>
+                Add a sentence of your own
+            </li>
+            <li>
+                Submit the story!
+            </li>
+        </ol>
+    </p>
+
+    <h2 class="heading2">Help Info</h2>
+    <p class ="aboutText">
+        Confused by how to use the site?
+        <a href="mailto:ministorymaker1@gmail.com">
+            Feel free to email us if you need any help!
+        </a>
+    </p>
+</section>
 
 <?php
     include('footer.php');
