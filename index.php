34c1a2534bb636cb0c906296bb2af0382b94f5f5
squidknits <sabertra@uvm.edu>
2019-05-04 19:14:24 -0400

    Added content to homepage

--- index.php
+++ index.php
@@ -3,25 +3,21 @@
 ?>
     <main>
         <article id="intro-article">
+            <img class="home-img-1" alt="" src="images/home-img-1.jpg">
             <p class='intro'>
-                <img class="home-img-1" alt="" src="images/home-img-1.jpg">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean 
-                finibus rhoncus nunc. Nulla facilisi. Ut ac gravida mauris, eu 
-                dictum sapien. Nam blandit tincidunt nisi ac euismod. Sed justo est,
-                gravida ut tellus ut, varius pellentesque tortor. Vestibulum laoreet 
-                ipsum et enim aliquet tincidunt. In vulputate leo eu magna faucibus 
-                suscipit. Mauris cursus erat ac malesuada rhoncus.
+                There are countless classic children's stories out there, 
+                all with their own themes and characters. But what would 
+                happen if these stories were combined in random, and sometimes 
+                funny ways? That's what Mini Story Maker is for! 
             </p>   
         </article>
         <article>
             <img class="home-img-2" alt="" src="images/home-img-2.jpg">
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean 
-                finibus rhoncus nunc. Nulla facilisi. Ut ac gravida mauris, eu 
-                dictum sapien. Nam blandit tincidunt nisi ac euismod. Sed justo est,
-                gravida ut tellus ut, varius pellentesque tortor. Vestibulum laoreet 
-                ipsum et enim aliquet tincidunt. In vulputate leo eu magna faucibus 
-                suscipit. Mauris cursus erat ac malesuada rhoncus.
+                Some stories will make sense, and some will be completely ridiculous! That's 
+                where you come in. Write a sentence or two to complete your new 
+                story and tie the plots together. Can you make the story make sense? 
+                <a href="makeastory.php">Try now!</a>
             </p>
         </article>
     </main>
