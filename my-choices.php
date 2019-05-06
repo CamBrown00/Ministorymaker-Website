7b0a03b800d2859d9a1d323c7abc899902bc783d
cameroo52 <ccbrown@uvm.edu>
2019-05-06 10:10:39 -0700

    Created the mychoices document, styled the mychoices page

---
+++ my-choices.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+
+        <title>Web Usability Test</title>
+
+        <meta charset="utf-8">
+        <meta name="author" content="Cameron Brown, AriaRay Brown, Sydney Bertrand">
+        <meta name='description' content="A summary of the choices we made">
+        
+        <link rel='stylesheet' href='css/cam.css' type='text/css' media='screen' />
+
+    </head>
+<body id="choices">
+    <section>
+        <h1>My Choices</h1>
+        <p>
+            During the planning phase for our project we initially set out to 
+            achieve a simple goal, create a website that offers a unique 
+            functionality, and we decided to go with Mini Story Maker. Given 
+            the volume of websites on the internet that revolve around providing 
+            functionality through some utility, there are probably a myriad of 
+            websites to fulfill any practical utility you could think of. So 
+            instead of building a website that could provide a tool to the user, 
+            we determined that the only way to stand out would be to offer a 
+            functionality on our website that provides entertainment instead of 
+            utility. We based the premise of the site on Mad Libs, added an 
+            element of random generation, and incorporated children's literature 
+            to focus more on the younger demographic. 
+        </p>
+        <p>
+            We styled the page with all of these facts in mind, and especially 
+            paid attention to making the site appealing to our target demographic. 
+            “Simple and comfortable” was the guiding phrase we used in styling 
+            each of the pages. Some aspects of the design that exemplify this 
+            are the minimal use of thin borders, frequent use of rounded edges, 
+            warm and muted colors, etc. We also utilized transitions as often 
+            as possible to keep everything looking smooth. 
+        </p>
+        <p>
+            There are countless classic children's stories out there, 
+            all with their own themes and characters. But what would 
+            happen if these stories were combined in random, and sometimes 
+            funny ways? That's what Mini Story Maker is for!
+        </p>
+    </section>
+</body>
+</html>
+
+
